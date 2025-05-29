967a1ced65cd33d811ea9333d2bc1f4914bb80c6
DCHani <haniben421@gmail.com>
2025-05-29 18:13:52 +0100

    Initial commit

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -15,6 +15,7 @@ const GlobalStyle = createGlobalStyle`
     -moz-osx-font-smoothing: grayscale;
     background-color: ${theme.colors.background};
     color: ${theme.colors.text};
+    
   }
 
   a {
